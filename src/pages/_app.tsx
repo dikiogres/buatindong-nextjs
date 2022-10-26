d9ab8bd91fee05bb07bf93347df5c9a243741caa
dikiogres <dikiogres@gmail.com>
2022-10-26 15:44:46 +0700

    feat: app pagep properties

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect, ReactNode } from 'react';
+import Router from 'next/router';
+import { initGA, logPageView } from '../analytics';
+import 'rc-tabs/assets/index.css';
+import 'swiper/swiper-bundle.min.css';
+import 'rc-drawer/assets/index.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface Props {
+  Component?: ReactNode
+  pageProps?: ReactNode
+}
+
+export default function CustomApp({ Component, pageProps }:Props) {
+  useEffect(() => {
+    initGA();
+    logPageView();
+    Router.events.on('routeChangeComplete', logPageView);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
