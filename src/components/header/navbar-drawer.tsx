78b1ed818659b38ef44f7d49d3f985747afcabcb
dikiogres <dikiogres@gmail.com>
2022-10-29 15:17:42 +0700

    feat: drawer component

---
+++ src/components/header/navbar-drawer.tsx
@@ -0,0 +1,17 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { IoMdMenu } from 'react-icons/io';
+import React, { useContext } from 'react';
+import { jsx, Box, Image } from 'theme-ui';
+import { Link } from 'react-scroll';
+import { DrawerContext } from 'contexts/drawer/drawer-context';
+import Drawer from 'components/drawer';
+import Logo from 'components/logo';
+import menuItems from './header.data';
+import close from 'assets/images/icons/close.png';
+
+const DrawerNav = () => {
+  
+};
+export default DrawerNav;
+
